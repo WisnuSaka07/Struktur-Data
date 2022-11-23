b819df72f1c3ff804b3cbcf2634a5ba7afa2fa68
WisnuSaka07 <113821220+WisnuSaka07@users.noreply.github.com>
2022-11-23 08:05:59 +0700

    Update Latihan_directedgraph.cpp

--- Latihan_directedgraph.cpp
+++ Latihan_directedgraph.cpp
@@ -1,80 +1,92 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <queue>
 using namespace std;
 
+class Graph {
+    protected:
+        int a;
+        list<int> adj[100];
+        int pred[100];
+        int dist[100];
 
-class Graph
-{
-	int V; 
+    public:
+        Graph(int len) {
+            a = len;
+        }
 
-	t<int>> adj;
-public:
-	Graph(int V); 
+        void addEdge(int b, int a){
+            adj[b].push_back(a);
+        }
 
-	
-	void addEdge(int v, int w);
+        void showGraph(){
+            for (int i = 0; i < a; i++)
+            {
+                list<int>::iterator it;
+                cout << i << " ";
+                for(it = adj[i].begin(); it != adj[i].end(); ++it){
+                    cout << *it << " ";
+                }
+                cout << endl;
+            }
+        }
 
-	
-	void BFS(int s);
+        void BFS(int start){
+            cout << "=======" << endl;
+            bool visited[100];
+            for (int i = 0; i < a; i++)
+            {
+                visited[i] = false;
+                pred[i] = -1;
+                dist[i] = -1;
+            }
+            
+            queue<int> q;
+            visited[start] = true;
+            dist[start] = 0;
+
+            list<int>::iterator it;
+            q.push(start);
+
+            while (!q.empty())
+            {
+                int curr = q.front();
+                q.pop();
+                for (it = adj[curr].begin(); it != adj[curr].end(); it++)
+                {
+                    int index = *it;
+                    if(!visited[index])
+                    {
+                        visited[index] = true;
+                        q.push(index);
+                        dist[index] = dist[curr] + 1;
+                        pred[index] = curr;
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < a; i++)
+            {
+                cout << dist[i] << " " << pred[i] << endl;
+            }
+            
+        }
 };
 
-Graph::Graph(int V)
-{
-	this->V = V;
-	adj.resize(V);
+
+int main(){
+    Graph graph1(5);
+
+    graph1.addEdge( 4, 2);
+    graph1.addEdge( 4, 3);
+    graph1.addEdge( 2, 1);
+    graph1.addEdge( 2, 3);
+    graph1.addEdge( 1, 3);
+    graph1.addEdge( 0, 1);
+    
+    graph1.showGraph();
+
+    graph1.BFS(0);
 }
-
-void Graph::addEdge(int v, int w)
-{
-	adj[v].push_back(w); 
-}
-
-void Graph::BFS(int s)
-{
-	
-	vector<bool> visited;
-	visited.resize(V,false);
-
-	
-	list<int> queue;
-
-	
-	visited[s] = true;
-	queue.push_back(s);
-
-	while(!queue.empty())
-	{
-		
-		s = queue.front();
-		cout << s << " ";
-		queue.pop_front();
-
-		
-		for (auto adjecent: adj[s])
-		{
-			if (!visited[adjecent])
-			{
-				visited[adjecent] = true;
-				queue.push_back(adjecent);
-			}
-		}
-	}
-}
-
-
-int main()
-{
-	Graph g(5);
-	g.addEdge(4, 2);
-	g.addEdge(4, 3);
-	g.addEdge(2, 1);
-	g.addEdge(2, 3);
-	g.addEdge(1, 3);
-	g.addEdge(0, 1);
-
-	cout << "Following is Breadth First Traversal "
-		<< "(starting from vertex 4) \n";
-	g.BFS(4);
-
-	return 0;
-}
-
