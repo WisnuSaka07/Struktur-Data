05a62ddc345f7191e280a586b56d8dbd33b68033
WisnuSaka07 <113821220+WisnuSaka07@users.noreply.github.com>
2022-12-06 23:21:15 +0700

    Update AdjacencyMatrixBFS.cpp

--- Final Project/AdjacencyMatrixBFS.cpp
+++ Final Project/AdjacencyMatrixBFS.cpp
@@ -67,27 +67,33 @@ class Graph {
 int main() 
 {
   Graph g(21);
-  g.addEdge(1, 2);
-  g.addEdge(2, 3);
-  g.addEdge(3, 4);
-  g.addEdge(4, 5);
-  g.addEdge(5, 6);
-  g.addEdge(6, 7);
-  g.addEdge(7, 8);
-  g.addEdge(8, 9);
-  g.addEdge(9, 10);
-  g.addEdge(10, 11);
-  g.addEdge(11, 12);
-  g.addEdge(12, 13);
-  g.addEdge(13, 14);
-  g.addEdge(14, 15);
-  g.addEdge(15, 16);
-  g.addEdge(16, 17);
-  g.addEdge(17, 18);
-  g.addEdge(18, 19);
-  g.addEdge(19, 20);
-  g.addEdge(20, 1);
-
-  g.bfs(1, 20);
-  //g.toString();
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 20);
+    g.addEdge(2, 4);
+    g.addEdge(3, 16);
+    g.addEdge(4, 5);
+    g.addEdge(4, 6);
+    g.addEdge(4, 16);
+    g.addEdge(5, 7);
+    g.addEdge(6, 10);
+    g.addEdge(6, 14);
+    g.addEdge(7, 8);
+    g.addEdge(8, 9);
+    g.addEdge(9, 11);
+    g.addEdge(10, 12);
+    g.addEdge(11, 12);
+    g.addEdge(12, 13);
+    g.addEdge(13, 14);
+    g.addEdge(14, 15);
+    g.addEdge(15, 16);
+    g.addEdge(15, 17);
+    g.addEdge(16, 18);
+    g.addEdge(17, 18);
+    g.addEdge(18, 19);
+    g.addEdge(19, 20);
+ 
+    g.bfs(0, 12);
+  
 }
